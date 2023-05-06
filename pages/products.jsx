dd8beb46c869517f2aa8bf3d3ff026385a2b51ae
SageKyle <ominyipaulo@gmail.com>
2023-05-07 00:09:59 +0100

    add a search button (not implemented)

--- pages/products.jsx
+++ pages/products.jsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { useState } from 'react';
 import Products from '../components/Products';
 import { baseUrl } from '../lib/data';
 
@@ -25,15 +26,26 @@ export async function getStaticProps() {
 }
 
 export default function products({ allProducts, error }) {
+	const [search, setSearch] = useState('');
+
 	return (
 		<>
 			<Head>
 				<title>Product Page</title>
 			</Head>
 			<main className="p-6 w-4/5 mx-auto ">
-				<nav className="flex mb-4 capitalize space-x-2">
+				<nav className="flex w-full items-center justify-start mb-4 capitalize gap-2">
 					<Link href="/">Home</Link>
 					<Link href="/">products</Link>
+					<form className="ml-auto inline-block mr-0 w-1/3 h-6 ">
+						<input
+							type="text"
+							placeholder="Search protucts..."
+							value={search}
+							onChange={(e) => setSearch(e.target.value)}
+							className="outline-none bg-transparent border-2 border-cyan-500 rounded p-1 text-sm font-light w-full"
+						/>
+					</form>
 				</nav>
 				<br />
 				<Products products={allProducts} error={error} />
